ad5de8e2eb1ac93610cd20ee9e9bb532f9960a7f
juninjune <46495192+juninjune@users.noreply.github.com>
2022-07-30 21:10:58 +0900

    added bookmarks

---
+++ js/apps.js
@@ -0,0 +1,59 @@
+const leftArrow = document.querySelector(".apps__left-arrow");
+const rightArrow = document.querySelector(".apps__right-arrow");
+
+const apps = document.getElementsByClassName("app");
+
+let currentAppIndex = 1;
+
+function handleLeftClick() {
+  console.log(currentAppIndex);
+  let nextAppIndex;
+  if (currentAppIndex === 0) {
+    nextAppIndex = apps.length - 1;
+  } else {
+    nextAppIndex = currentAppIndex - 1;
+  }
+  leftArrow.removeEventListener("click", handleLeftClick);
+  rightArrow.removeEventListener("click", handleRightClick);
+
+  apps[currentAppIndex].classList.add("disappear");
+  apps[nextAppIndex].classList.add("disappear");
+  setTimeout(() => {
+    apps[currentAppIndex].classList.add("hidden");
+    apps[currentAppIndex].classList.remove("disappear");
+    apps[nextAppIndex].classList.remove("hidden");
+  }, 500);
+  setTimeout(() => {
+    apps[nextAppIndex].classList.remove("disappear");
+    leftArrow.addEventListener("click", handleLeftClick);
+    rightArrow.addEventListener("click", handleRightClick);
+    currentAppIndex = nextAppIndex;
+  }, 600);
+}
+function handleRightClick() {
+  let nextAppIndex;
+  if (currentAppIndex === apps.length - 1) {
+    nextAppIndex = 0;
+  } else {
+    nextAppIndex = currentAppIndex + 1;
+  }
+  leftArrow.removeEventListener("click", handleLeftClick);
+  rightArrow.removeEventListener("click", handleRightClick);
+
+  apps[nextAppIndex].classList.add("disappear");
+  apps[currentAppIndex].classList.add("disappear");
+  setTimeout(() => {
+    apps[currentAppIndex].classList.add("hidden");
+    apps[currentAppIndex].classList.remove("disappear");
+    apps[nextAppIndex].classList.remove("hidden");
+  }, 500);
+  setTimeout(() => {
+    apps[nextAppIndex].classList.remove("disappear");
+    leftArrow.addEventListener("click", handleLeftClick);
+    rightArrow.addEventListener("click", handleRightClick);
+    currentAppIndex = nextAppIndex;
+  }, 600);
+}
+
+leftArrow.addEventListener("click", handleLeftClick);
+rightArrow.addEventListener("click", handleRightClick);
